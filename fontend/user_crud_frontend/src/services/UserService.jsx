eddc667ba1e44365bb84f28bb940ba32f95803de
Radha <122531010+bhaskarradha123@users.noreply.github.com>
2025-08-07 13:56:14 +0530

    Update UserService.jsx

--- fontend/user_crud_frontend/src/services/UserService.jsx
+++ fontend/user_crud_frontend/src/services/UserService.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://user-crud-backend-53a3.onrender.com',
 });
 
 // Add JWT interceptor
